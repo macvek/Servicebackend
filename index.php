ded519c9982f6aa020cb66430d2e68d26f397a3f
Maciej Aleksandrowicz <maciejaleksandrowicz@almatron.pl>
2016-09-22 04:03:23 +0200

    sync

--- index.php
+++ index.php
@@ -5,40 +5,42 @@
 <title>Service backend @</title>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Poiret+One|Quicksand" rel="stylesheet">
 <body>
-    <div class="pluginbox">
-        <div class="pluginbox-title">uptime</div>
-        <div class="pluginbox-content">2:13  up 1 day,  4:17, 3 users, load averages: 2,10 2,27 1,97</div>
-    </div>
-    <div class="pluginbox">
-        <div class="pluginbox-title">control_panel</div>
-        <div class="pluginbox-content">
-            <table class="withvalues">
-                <tr>
-                    <td class="labelcol">refresh_rate</td>
-                    <td class="valuecol"><input></input></td>
-                </tr>
-                <tr>
-                    <td class="labelcol">fade_on_update</td>
-                    <td class="valuecol"><input class="" type="checkbox"></input></td>
-                </tr>
-                <tr>
-                    <td class="labelcol">console_autoscroll</td>
-                    <td class="valuecol"><input id="control_console_autoscroll" type="checkbox"></input></td>
-                </tr>
-                <tr>
-                    <td class="labelcol">customize_panes</td>
-                    <td class="valuecol"><input id="control_customize_panes" type="checkbox"></input></td>
-                </tr>
-            </table>
+    <div>
+        <div class="pluginbox">
+            <div class="pluginbox-title">uptime</div>
+            <div class="pluginbox-content">2:13  up 1 day,  4:17, 3 users, load averages: 2,10 2,27 1,97</div>
         </div>
-    </div>
-    <div class="pluginbox plugin-console" >
-        <div class="pluginbox-buttons">
-            <span class="clear-console">&#10008;</span>
+        <div class="pluginbox">
+            <div class="pluginbox-title">control_panel</div>
+            <div class="pluginbox-content">
+                <table class="withvalues">
+                    <tr>
+                        <td class="labelcol">refresh_rate</td>
+                        <td class="valuecol"><input></input></td>
+                    </tr>
+                    <tr>
+                        <td class="labelcol">fade_on_update</td>
+                        <td class="valuecol"><input class="" type="checkbox"></input></td>
+                    </tr>
+                    <tr>
+                        <td class="labelcol">console_autoscroll</td>
+                        <td class="valuecol"><input id="control_console_autoscroll" type="checkbox"></input></td>
+                    </tr>
+                    <tr>
+                        <td class="labelcol">customize_panes</td>
+                        <td class="valuecol"><input id="control_customize_panes" type="checkbox"></input></td>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <div class="pluginbox-title">console</div>
-        <div class="pluginbox-content scrollable-console">
+        <div class="pluginbox plugin-console" >
+            <div class="pluginbox-buttons">
+                <span class="clear-console">&#10008;</span>
+            </div>
+            <div class="pluginbox-title">console</div>
+            <div class="pluginbox-content scrollable-console">
 
+            </div>
         </div>
     </div>
     
@@ -109,6 +111,11 @@
             cursor:pointer;
         }
 
+        .panes-manager {
+            margin-right:5px;
+            color:#999;
+        }
+
     </style>
     <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
     <script src="jquery.color.js"></script>
@@ -122,6 +129,91 @@
                 MessageBroker.send("serviceconsole.autoscroll",$(this).prop("checked"));
             });
 
+            function doMoveUp() {
+                var pluginBox = parentPluginBox(this);
+                if (0 === pluginBox.prev().length) { return;}
+                var params = animParams(pluginBox);
+                moveFirstAfterSecond(pluginBox.prev(), pluginBox);
+                animateTransition(params,pluginBox);
+            }
+
+            function doMoveDown() {
+                var pluginBox = parentPluginBox(this);
+                if (0 === pluginBox.next().length) { return;}
+                var params = animParams(pluginBox);
+                moveFirstAfterSecond(pluginBox, pluginBox.next());
+                animateTransition(params,pluginBox);
+            }
+
+            function parentPluginBox(actionButton) {
+                return $(actionButton).closest(".pluginbox")
+            }
+
+            function moveFirstAfterSecond(first, second) {               
+                first.detach().insertAfter(second);
+            }
+
+            function animParams(from) {
+                var offset = $(from).offset();
+
+                return {
+                    top: offset.top,
+                    left: offset.left,
+                    width: $(from).width(),
+                    height: $(from).height(),
+                    border: $(from).css("border"),
+                    borderRadius: $(from).css("border-radius"),
+                    padding: $(from).css("padding")
+                };
+            }
+
+            function animateTransition(fromParam,to) {
+                var animBox = $("<div>").css({
+                    position:'absolute',
+                    top: fromParam.top,
+                    left:fromParam.left,
+                    width:fromParam.width,
+                    height:fromParam.height,
+                    backgroundColor:"#fff",
+                    border: fromParam.border,
+                    borderRadius: fromParam.borderRadius,
+                    padding:fromParam.padding,
+                    opacity:0.5
+                })
+
+
+                var toOffset = to.offset();
+                var toSize = {width: to.width(), height: to.height()};
+                $("body").append(animBox);
+                animBox.animate({
+                    top:toOffset.top,
+                    left:toOffset.left,
+                    width:toOffset.width,
+                    height:toOffset.height,
+                    opacity:0
+                }, {complete: function() { animBox.remove(); }});
+            }
+
+            function prependPanesManager() {
+                var panesManager = $("<div>").addClass("panes-manager pluginbox-buttons");
+                
+                var moveUp = $("<span>").html("&#9650;").click(doMoveUp);
+                var moveDown = $("<span>").html("&#9660;").click(doMoveDown);
+
+                panesManager.append(moveUp).append(" ").append(moveDown);
+
+                $(this).prepend(panesManager);
+
+            }
+
+            $("#control_customize_panes").change(function() {
+                if ($(this).prop("checked")) {
+                    $(".pluginbox").each(prependPanesManager);
+                }
+                else {
+                    $(".pluginbox .panes-manager").remove();
+                }
+            });
             MessageBroker.broadcast("appstart");
         });
     </script>
