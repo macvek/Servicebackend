53b9a59cb16c4f36ae5893a8cdb85b0c5f35bdbe
Maciej Aleksandrowicz <maciejaleksandrowicz@almatron.pl>
2016-10-08 11:09:03 +0200

    campreview from snapshot

--- campreview.php
+++ campreview.php
@@ -1,6 +1,32 @@
 <?php
 require 'security.php';
 
+if (!is_dir("snapshots")) {
+    die("snapshots not supported");
+}
+
+$num = "empty";
+
+$dir = opendir("snapshots");
+while (($file = readdir($dir)) !== false) {
+    if (strpos($file,"snap") !== 0) {
+        continue;
+    }
+
+    $numPart = substr($file,4,8);
+    $eachNum = intval($numPart);
+    if ("empty" === $num || $eachNum > $num) {
+        $num = $eachNum;
+        $numName = $numPart;
+    }
+}
+closedir($dir);
+$fullname = "snapshots/snap$numName.jpeg";
+
+if ("empty" === $num || !file_exists($fullname)) {
+    die("no snapshots found");
+}
+
 header('Content-Type: image/jpeg');
 header("Cache-Control: no-cache, must-revalidate");
-readfile("snapshot.jpeg");
+readfile($fullname);
